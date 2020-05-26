e9c4e77def11e4fd59a661544c7f5f43cacedc78
jijo0465 <jijo0465@gmail.com>
2020-05-26 21:48:22 +0530

    call record

--- lib/screens/call.dart
+++ lib/screens/call.dart
@@ -1,8 +1,14 @@
 import 'dart:async';
+import 'dart:convert';
+import 'dart:ui';
+
 import 'package:agora_rtc_engine/agora_rtc_engine.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:teacher_app/components/digicampus_appbar.dart';
 import 'package:teacher_app/components/live_stream_settings.dart';
+import 'package:teacher_app/models/grade.dart';
+import 'package:http/http.dart' as http;
 
 class CallPage extends StatefulWidget {
   /// non-modifiable channel name of the page
@@ -21,7 +27,14 @@ class _CallPageState extends State<CallPage> {
   bool muted = false;
   final broadcasterUid = 3001;
   final bool isflag = false;
-
+  bool checkParticipants = false;
+  // Firestore firestore = Firestore.instance;
+  // List<DocumentSnapshot> _items;
+  // DocumentSnapshot _item;
+  Grade grade = Grade.empty();
+  int id = 4001;
+  String resourceId;
+  String sid;
   @override
   void dispose() {
     // clear users
@@ -35,6 +48,7 @@ class _CallPageState extends State<CallPage> {
   @override
   void initState() {
     super.initState();
+    grade.setId(id);
     // initialize agora sdk
     initialize();
   }
@@ -64,8 +78,12 @@ class _CallPageState extends State<CallPage> {
   Future<void> _initAgoraRtcEngine() async {
     await AgoraRtcEngine.create(APP_ID);
     await AgoraRtcEngine.enableVideo();
-    await AgoraRtcEngine.joinChannel(null, 'live', null, broadcasterUid);
-
+    await AgoraRtcEngine.joinChannel(
+        "006afe8cf8cb8fd494bbd8d7e1d900483baIAByyYgio6HHPtqn6oKpjpQmgdG0BTBhgX5vxbDCOE7b+q8sD1MAAAAAEABfqfzlUUfNXgEAAQBRR81e",
+        'live',
+        null,
+        broadcasterUid);
+        startRecording();
     // await AgoraRtcEngine.enableWebSdkInteroperability(true);
   }
 
@@ -76,6 +94,11 @@ class _CallPageState extends State<CallPage> {
         final info = 'onError: $code';
         _infoStrings.add(info);
       });
+      Future.delayed(Duration(seconds: 3)).then((value) {
+        setState(() {
+          _infoStrings.removeLast();
+        });
+      });
     };
 
     AgoraRtcEngine.onJoinChannelSuccess = (
@@ -83,9 +106,24 @@ class _CallPageState extends State<CallPage> {
       int uid,
       int elapsed,
     ) {
+      
+      
       setState(() {
         final info = 'onJoinChannel: $channel, uid: $uid';
         _infoStrings.add(info);
+      });
+
+      //   DocumentReference documentReference =
+      //     firestore.collection('classroom_${grade.id}').document('live_session');
+      // firestore.runTransaction((transaction) async {
+      //   await transaction.update(
+      //       documentReference, {'userid': FieldValue.arrayUnion([uid])});
+      // });
+
+      Future.delayed(Duration(seconds: 3)).then((value) {
+        setState(() {
+          _infoStrings.removeLast();
+        });
       });
     };
 
@@ -94,13 +132,24 @@ class _CallPageState extends State<CallPage> {
         _infoStrings.add('onLeaveChannel');
         _users.clear();
       });
+      Future.delayed(Duration(seconds: 3)).then((value) {
+        setState(() {
+          _infoStrings.removeLast();
+        });
+      });
     };
 
     AgoraRtcEngine.onUserJoined = (int uid, int elapsed) {
       setState(() {
         final info = 'userJoined: $uid';
         _infoStrings.add(info);
-        if (isflag) _users.add(uid);
+        _users.add(uid);
+      });
+      print(_users[0]);
+      Future.delayed(Duration(seconds: 3)).then((value) {
+        setState(() {
+          _infoStrings.removeLast();
+        });
       });
     };
 
@@ -110,6 +159,11 @@ class _CallPageState extends State<CallPage> {
         _infoStrings.add(info);
         _users.remove(uid);
       });
+      Future.delayed(Duration(seconds: 3)).then((value) {
+        setState(() {
+          _infoStrings.removeLast();
+        });
+      });
     };
 
     AgoraRtcEngine.onFirstRemoteVideoFrame = (
@@ -118,9 +172,15 @@ class _CallPageState extends State<CallPage> {
       int height,
       int elapsed,
     ) {
+      print("oihsidogisidggdsggggggggggggg");
       setState(() {
         final info = 'firstRemoteVideo: $uid ${width}x $height';
         _infoStrings.add(info);
+      });
+      Future.delayed(Duration(seconds: 3)).then((value) {
+        setState(() {
+          _infoStrings.removeLast();
+        });
       });
     };
   }
@@ -191,8 +251,7 @@ class _CallPageState extends State<CallPage> {
   Widget _viewVideo() {
     return Container(
       child: AgoraRtcEngine.createNativeView((viewId) {
-        AgoraRtcEngine.setupLocalVideo(
-            viewId, VideoRenderMode.Fit);
+        AgoraRtcEngine.setupLocalVideo(viewId, VideoRenderMode.Fit);
       }),
     );
     //  AgoraRtcEngine.createNativeView((viewId) {
@@ -210,47 +269,113 @@ class _CallPageState extends State<CallPage> {
   /// Toolbar layout
   Widget _toolbar() {
     return Container(
+      width: MediaQuery.of(context).size.width,
       alignment: Alignment.bottomCenter,
       padding: const EdgeInsets.symmetric(vertical: 48),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: <Widget>[
-          RawMaterialButton(
-            onPressed: _onToggleMute,
-            child: Icon(
-              muted ? Icons.mic_off : Icons.mic,
-              color: muted ? Colors.white : Colors.blueAccent,
-              size: 20.0,
-            ),
-            shape: CircleBorder(),
-            elevation: 2.0,
-            fillColor: muted ? Colors.blueAccent : Colors.white,
-            padding: const EdgeInsets.all(12.0),
+          Flexible(
+            flex: 2,
+            child: RaisedButton(
+                shape: CircleBorder(side: BorderSide(color: Colors.black12)),
+                color: Theme.of(context).primaryColor.withOpacity(0.4),
+                onPressed: checkParticipants ? null : () {},
+                child: Icon(
+                  Icons.chat,
+                  color: Colors.white,
+                  size: 20.0,
+                )),
           ),
-          RawMaterialButton(
-            onPressed: () => _onCallEnd(context),
-            child: Icon(
-              Icons.call_end,
-              color: Colors.white,
-              size: 35.0,
-            ),
-            shape: CircleBorder(),
-            elevation: 2.0,
-            fillColor: Colors.redAccent,
-            padding: const EdgeInsets.all(15.0),
+          Flexible(
+            flex: 2,
+            child: RaisedButton(
+                shape: CircleBorder(side: BorderSide(color: Colors.black12)),
+                color: Theme.of(context).primaryColor.withOpacity(0.4),
+                onPressed: checkParticipants ? null : _onToggleMute,
+                child: Icon(
+                  muted ? Icons.mic_off : Icons.mic,
+                  color: muted ? Colors.red : Colors.white,
+                  size: 20,
+                )),
           ),
-          RawMaterialButton(
-            onPressed: _onSwitchCamera,
-            child: Icon(
-              Icons.switch_camera,
-              color: Colors.blueAccent,
-              size: 20.0,
+          Flexible(
+            flex: 3,
+            child: RaisedButton(
+              shape: CircleBorder(side: BorderSide(color: Colors.black12)),
+              color: Theme.of(context).primaryColor.withOpacity(0.4),
+              onPressed: checkParticipants ? null : () => _onCallEnd(context),
+              child: Icon(
+                Icons.call_end,
+                color: Colors.red,
+                size: 40.0,
+              ),
             ),
-            shape: CircleBorder(),
-            elevation: 2.0,
-            fillColor: Colors.white,
-            padding: const EdgeInsets.all(12.0),
-          )
+          ),
+          Flexible(
+            flex: 2,
+            child: RaisedButton(
+                shape: CircleBorder(side: BorderSide(color: Colors.black12)),
+                color: Theme.of(context).primaryColor.withOpacity(0.4),
+                onPressed: checkParticipants ? null : _onSwitchCamera,
+                child: Icon(
+                  Icons.switch_camera,
+                  color: Colors.white,
+                  size: 20.0,
+                )),
+          ),
+          Flexible(
+            flex: 2,
+            child: RaisedButton(
+                shape: CircleBorder(side: BorderSide(color: Colors.black12)),
+                color: Theme.of(context).primaryColor.withOpacity(0.4),
+                onPressed: () {
+                  setState(() {
+                    checkParticipants = !checkParticipants;
+                  });
+                },
+                child: Icon(
+                  Icons.group,
+                  color: Colors.white,
+                  size: 20.0,
+                )),
+          ),
+          //   RawMaterialButton(
+          //     onPressed: _onToggleMute,
+          //     child: Icon(
+          //       muted ? Icons.mic_off : Icons.mic,
+          //       color: muted ? Colors.white : Colors.blueAccent,
+          //       size: 20.0,
+          //     ),
+          //     shape: CircleBorder(),
+          //     elevation: 2.0,
+          //     fillColor: muted ? Colors.blueAccent : Colors.white,
+          //     padding: const EdgeInsets.all(12.0),
+          //   ),
+          //   RawMaterialButton(
+          //     onPressed: () => _onCallEnd(context),
+          //     child: Icon(
+          //       Icons.call_end,
+          //       color: Colors.white,
+          //       size: 35.0,
+          //     ),
+          //     shape: CircleBorder(),
+          //     elevation: 2.0,
+          //     fillColor: Colors.redAccent,
+          //     padding: const EdgeInsets.all(15.0),
+          //   ),
+          //   RawMaterialButton(
+          //     onPressed: _onSwitchCamera,
+          //     child: Icon(
+          //       Icons.switch_camera,
+          //       color: Colors.blueAccent,
+          //       size: 20.0,
+          //     ),
+          //     shape: CircleBorder(),
+          //     elevation: 2.0,
+          //     fillColor: Colors.white,
+          //     padding: const EdgeInsets.all(12.0),
+          //   )
         ],
       ),
     );
@@ -259,7 +384,7 @@ class _CallPageState extends State<CallPage> {
   /// Info panel to show logs
   Widget _panel() {
     return Container(
-      padding: const EdgeInsets.symmetric(vertical: 48),
+      padding: const EdgeInsets.symmetric(vertical: 60),
       alignment: Alignment.bottomCenter,
       child: FractionallySizedBox(
         heightFactor: 0.5,
@@ -287,12 +412,13 @@ class _CallPageState extends State<CallPage> {
                           horizontal: 5,
                         ),
                         decoration: BoxDecoration(
-                          color: Colors.yellowAccent,
+                          color:
+                              Theme.of(context).primaryColor.withOpacity(0.3),
                           borderRadius: BorderRadius.circular(5),
                         ),
                         child: Text(
                           _infoStrings[index],
-                          style: TextStyle(color: Colors.blueGrey),
+                          style: TextStyle(color: Colors.white),
                         ),
                       ),
                     )
@@ -307,6 +433,7 @@ class _CallPageState extends State<CallPage> {
   }
 
   void _onCallEnd(BuildContext context) {
+    stopRecording();
     Navigator.pop(context);
   }
 
@@ -333,14 +460,111 @@ class _CallPageState extends State<CallPage> {
             _panel(),
             _toolbar(),
             DigiCampusAppbar(
-            icon: Icons.close,
-            onDrawerTapped: () {
-              Navigator.of(context).pop();
-            },
-          ),
+              icon: Icons.close,
+              onDrawerTapped: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            // StreamBuilder<QuerySnapshot>(
+            // stream:
+            //     firestore.collection('classroom_${grade.id}').snapshots(),
+            // builder: (BuildContext context,
+            //     AsyncSnapshot<QuerySnapshot> snapshot) {
+            //   // if (!snapshot.hasData)
+            //   if (!snapshot.hasData)
+            //     return
+            //   Center(
+            //     child: CircularProgressIndicator(
+            //       valueColor: AlwaysStoppedAnimation<Color>(
+            //           Theme.of(context).primaryColor),
+            //     ),
+            //   );
+            // else {
+            //   _items = snapshot.data.documents;
+            //   for (int i = 0; i < _items.length; i++)
+            //     if (_items[i].documentID == 'live_session')
+            //       _item = _items[i];
+            //   return
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Center(
+                child: ClipRect(
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(
+                        sigmaX: checkParticipants ? 25 : 0,
+                        sigmaY: checkParticipants ? 25 : 0),
+                    child: checkParticipants
+                        ? Container(
+                            width: MediaQuery.of(context).size.width - 40,
+                            height: MediaQuery.of(context).size.height * 0.6,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(12),
+                              color: Colors.black
+                                  .withOpacity(checkParticipants ? 0.4 : 0.0),
+                            ),
+                            child: _users.length == 0
+                                ? Text('No Users Joined!',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 16))
+                                : Column(
+                                    children:
+                                        //   List.generate(
+                                        //       _item['userid'].length, (index) {
+                                        // return Text('User ${index+1} : ${_item['userid'][index]}',
+                                        List.generate(_users.length, (index) {
+                                    return Text(
+                                        'User ${index + 1} : ${_users.elementAt(index)}',
+                                        style: TextStyle(
+                                            color: Colors.white, fontSize: 16));
+                                  }))
+                            // Center(
+                            //     child: Text(
+                            //   'No Participants yet!',
+                            //   style: TextStyle(
+                            //       color: Colors.white, fontSize: 16),
+                            // ))
+                            // Container(
+                            //     // width: 300,
+                            //     // color: Colors.white.withOpacity(0.3),
+                            //     ),
+                            )
+                        : Container(),
+                  ),
+                ),
+              ),
+            )
+            //   }
+            // })
           ],
         ),
       ),
     );
+  }
+
+  Future<void> startRecording() async {
+    print("Starting Recording");
+    String url = 'http://192.168.0.12:8080/start_recording';
+    Map<String, String> headers = {"Content-type": "application/json"};
+    Map<String, dynamic> params = {};
+    String data = jsonEncode(params);
+    
+    http.get(url, headers: headers).then((response) {
+      // print(response.body);
+      // resourceId = json.decode(response.body)['resourceId'];
+      // sid = json.decode(response.body)['resourceId'];
+    }).catchError((error) => print(error));
+  }
+
+  Future<void> stopRecording() async {
+    print('Stopping Recording....');
+    String url = 'http://192.168.0.12:8080/stop_recording';
+    Map<String, String> headers = {"Content-type": "application/json"};
+    Map<String, dynamic> params = {"resourceId": "$resourceId","sid":"$sid"};
+    String data = jsonEncode(params);
+    await http.get(url, headers: headers).then((response) {
+      print(response.body);
+      
+    }).catchError((error) => print(error));
   }
 }
